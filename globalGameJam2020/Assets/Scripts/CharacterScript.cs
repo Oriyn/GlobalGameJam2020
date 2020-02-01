5a137005973db9071307e90b703b00c00430b735
Oriyn <gabehagg@gmail.com>
2020-02-01 13:42:10 -0600

    Added sprites and animations

--- globalGameJam2020/Assets/Scripts/CharacterScript.cs
+++ globalGameJam2020/Assets/Scripts/CharacterScript.cs
@@ -9,11 +9,15 @@ public class CharacterScript : MonoBehaviour
     public Rigidbody2D rdb2d;
     private bool isMoving;
     private bool isGrounded;
+    public Animator animator;
+    public SpriteRenderer mySpriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         rdb2d = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -41,19 +45,32 @@ public class CharacterScript : MonoBehaviour
         {
             isMoving = true;
             rdb2d.velocity = new Vector2(rdb2d.velocity.x, jump);
+            animator.SetFloat("Speed", jump);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             isMoving = true;
+            mySpriteRenderer.flipX = false;
+            animator.SetFloat("Speed", velocity);
             rdb2d.velocity = new Vector2(velocity, rdb2d.velocity.y);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             isMoving = true;
+            mySpriteRenderer.flipX = true;
+            animator.SetFloat("Speed", velocity);
             rdb2d.velocity = new Vector2(-velocity, rdb2d.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isShooting", true);
+        } else
+        {
+            animator.SetBool("isShooting", false);
         }
         if (isMoving == false)
         {
+            animator.SetFloat("Speed", 0);
             rdb2d.velocity = new Vector2(0, rdb2d.velocity.y);
         }
     }
